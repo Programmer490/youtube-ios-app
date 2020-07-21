a3cf2072dfc11a45f516a1ec0de5cb0663a691d0
Programmer490 <Programmer490@gmail.com>
2020-07-21 13:41:34 -0600

    Added code to get API data

--- YouTube-iOS-App/Model.swift
+++ YouTube-iOS-App/Model.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class Model {
+    
+    func getVideos() {
+        
+    //Create a URL object
+        let url = URL(string: Constants.API_URL)
+        
+        guard url != nil else {
+            return
+        }
+    //Get a URLSession object
+        let session = URLSession.shared
+    //Get a data task from URLSession
+        let dataTask = session.dataTask(with: url!) { (data, resoponse, error) in
+            if error != nil || data == nil {
+                return
+            }
+            
+            // parsing the data into video objects
+        }
+    //Kick off the Task
+        dataTask.resume()
+    }
+    
+}
