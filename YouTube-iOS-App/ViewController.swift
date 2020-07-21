0f851a6f8819aebcd3f3955b91530aa09e64d241
Programmer490 <Programmer490@gmail.com>
2020-07-21 00:08:48 -0600

    Initial XCode Project

---
+++ YouTube-iOS-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube-iOS-App
+//
+//  Created by Joseph Manguso on 7/20/20.
+//  Copyright © 2020 Joseph Manguso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
