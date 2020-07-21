101428b75ef18070d80162ca255c0064d2f0c906
Programmer490 <Programmer490@gmail.com>
2020-07-21 13:40:19 -0600

    added basic code for getting API data

---
+++ YouTube-iOS-App/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  YouTube-iOS-App
+//
+//  Created by Joseph Manguso on 7/21/20.
+//  Copyright © 2020 Joseph Manguso. All rights reserved.
+//
+
+import Foundation
---
+++ YouTube-iOS-App/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  YouTube-iOS-App
+//
+//  Created by Joseph Manguso on 7/21/20.
+//  Copyright © 2020 Joseph Manguso. All rights reserved.
+//
+
+import Foundation
